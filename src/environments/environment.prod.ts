965f7f420a73c82965b3e87c7d2594de3adbeaf6
TheQuaker <cs2180008@di.uoa.gr>
2021-11-26 14:07:35 +0200

    added margin and material icons at form fields

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  beta: false,
+  MATOMO_URL: '',
+  MATOMO_SITE: -1,
+  API_ENDPOINT: '/catalogue',
+  API_TOKEN_ENDPOINT: '',
+  projectName: 'Observatory',
+  projectMail: 'example@oac.eu',
+  serviceORresource: 'Resource',
+  hasUserConsent: true,
+  showHelpContent: false,
+  privacyPolicyURL: '',
+  marketplaceBaseURL: ''
 };
