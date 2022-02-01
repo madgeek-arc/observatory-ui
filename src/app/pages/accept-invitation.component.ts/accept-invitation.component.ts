72b619f017efe0255d5bb4b31969c0bbd7bc701d
TheQuaker <cs2180008@di.uoa.gr>
2022-02-01 12:37:28 +0200

    minor changes for accept invitation component

--- src/app/pages/accept-invitation.component.ts/accept-invitation.component.ts
+++ src/app/pages/accept-invitation.component.ts/accept-invitation.component.ts
@@ -5,28 +5,34 @@ import {SurveyService} from "../../services/survey.service";
 
 @Component({
   selector: 'app-accept-invitation',
-  template: '<div class="whiteFilm">\n' +
-    '  <i class="loader-big" aria-hidden="true"></i>\n' +
-    '</div>'
+  templateUrl: 'accept-invitation.component.html'
 })
 
 export class AcceptInvitationComponent implements OnInit {
 
   token: string = null;
+  loading: boolean = false;
+  message: string = 'You are being registered';
+  error: string = null;
 
   constructor(private route: ActivatedRoute, private router: Router, private surveyService: SurveyService) {
   }
 
   ngOnInit() {
+    this.loading = true
     this.route.params.subscribe( params => {
       this.token = params['invitationToken'];
       if (this.token) {
         this.surveyService.acceptInvitation(this.token).subscribe(
           res => {
+            this.loading = false;
             this.router.navigate(['/']);
           },
           error => {
-            console.log(error);
+            this.message = 'Something went wrong, server replied: ';
+            this.error = error;
+            this.loading = false;
+            console.error(error);
           });
       }
     });
