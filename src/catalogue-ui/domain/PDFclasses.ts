048fe10996f36c42567043f019d82002b2edce0f
TheQuaker <cs2180008@di.uoa.gr>
2022-08-01 17:08:59 +0300

    more pdf testing

--- src/catalogue-ui/domain/PDFclasses.ts
+++ src/catalogue-ui/domain/PDFclasses.ts
@@ -1,11 +1,11 @@
 export class DocDefinition {
   header: Content;
-  content: Content[];
+  content: any[];
   styles: Object;
-
+  images: Object;
 
   constructor() {
-    this.header = new Content('', '');
+    this.header = new Content('', ['']);
     this.content = [];
     this.styles = {};
   }
@@ -13,11 +13,34 @@ export class DocDefinition {
 
 export class Content {
   text: string;
-  style: string;
+  style: string[];
+  width: string;
 
-
-  constructor(text: string, style: string) {
+  constructor(text: string, style: string[]) {
     this.text = text;
+    this.style = style;
+  }
+}
+
+export class Columns {
+  columns: any[];
+
+
+  constructor() {
+    this.columns = [];
+  }
+}
+
+export class PdfImage {
+  image: string;
+  height: number;
+  width: number;
+  style: string[];
+
+  constructor(image: string, height: number, width: number, style: string[]) {
+    this.image = image;
+    this.height = height;
+    this.width = width;
     this.style = style;
   }
 }
