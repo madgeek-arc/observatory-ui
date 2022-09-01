bbbc6ad54527566d5971d501a4ccf99287e48f31
jbalasis <cs2180008@di.uoa.gr>
2022-09-01 14:33:32 +0300

    minor code cleaning

--- src/app/pages/contributions-dashboard/my-surveys/survey-form/survey-form.component.ts
+++ src/app/pages/contributions-dashboard/my-surveys/survey-form/survey-form.component.ts
@@ -3,7 +3,6 @@ import {ChapterEditComponent} from "../../../../../catalogue-ui/pages/dynamic-fo
 import {SurveyService} from "../../../../services/survey.service";
 import {Subscriber} from "rxjs";
 import {ActivatedRoute, Route, Router} from "@angular/router";
-import {Stakeholder} from "../../../../domain/userInfo";
 import {SurveyAnswer} from "../../../../domain/survey";
 import {UserService} from "../../../../services/user.service";
 import {zip} from "rxjs/internal/observable/zip";
@@ -19,14 +18,12 @@ export class SurveyFormComponent implements OnInit, OnDestroy {
   @ViewChild(ChapterEditComponent) child: ChapterEditComponent
 
   subscriptions = [];
-  currentGroup: Stakeholder = null;
   tabsHeader: string = null;
   survey: Model = null;
   surveyAnswers: SurveyAnswer = null
   surveyId: string = null;
   stakeholderId: string = null;
   ready = false;
-  printPDf = false;
 
   constructor(private surveyService: SurveyService, private userService: UserService, private route: ActivatedRoute,
               private router: Router) {}
@@ -34,8 +31,6 @@ export class SurveyFormComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.ready = false;
     this.tabsHeader = 'Sections';
-    if (this.router.url.includes('printPdf'))
-      this.printPDf = true;
 
     this.subscriptions.push(
       this.route.params.subscribe(params => {
