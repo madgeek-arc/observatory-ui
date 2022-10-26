bab3fd23c60f6320f86a60d0c0c6c0d6a8fd4b14
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2022-10-26 11:41:43 +0300

    Temporary commit on the new dashboard style

--- src/app/pages/national-contributions-to-eosc-dashboard/national-contributions-to-eosc-dashboard.component.ts
+++ src/app/pages/national-contributions-to-eosc-dashboard/national-contributions-to-eosc-dashboard.component.ts
@@ -2,10 +2,13 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-national-contributions-to-eosc-dashboard',
-  templateUrl: './national-contributions-to-eosc-dashboard.component.html'
+  templateUrl: './national-contributions-to-eosc-dashboard.component.html',
+  styleUrls: ['../../../app/shared/sidemenudashboard/side-menu-dashboard.component.css','./national-contributions-to-eosc-dashboard.component.css']
 })
 
 export class NationalContributionsToEOSCDashboardComponent {
+
+  open: boolean = true;
 
   constructor() {
   }
