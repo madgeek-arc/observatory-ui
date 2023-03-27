04a0694d0ea9d792a9fd387cf2be6638f135ecfb
Konstantinos Spyrou <spyroukon@gmail.com>
2023-03-27 16:19:48 +0300

    refactoring

--- init.sh
+++ init.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # Substitute Matomo Properties
-MAIN_FILE=$(echo "/usr/share/nginx/html/$(ls /usr/share/nginx/html/ | grep -e 'main.')")
-MAIN_TMP=$(echo "$MAIN_FILE.tmp")
-echo "main.js = $MAIN_FILE | main-tmp: $MAIN_TMP"
-cp $MAIN_FILE $MAIN_TMP
-envsubst '${MATOMO_URL} ${MATOMO_SITE_ID}' < $MAIN_TMP > $MAIN_FILE
+MAIN_PROXY_CONF_FILE=$(echo "/usr/share/nginx/html/$(ls /usr/share/nginx/html/ | grep -e 'main.')")
+MAIN_TMP=$(echo "$MAIN_PROXY_CONF_FILE.tmp")
+echo "main.js = $MAIN_PROXY_CONF_FILE | main-tmp: $MAIN_TMP"
+cp $MAIN_PROXY_CONF_FILE $MAIN_TMP
+envsubst '${MATOMO_URL} ${MATOMO_SITE_ID}' < $MAIN_TMP > $MAIN_PROXY_CONF_FILE
 rm $MAIN_TMP
 
 # Create Nginx configuration
 CONF_TMPL=/etc/nginx/nginx.conf.txt
-FILE=/etc/nginx/conf.d/nginx.conf
+PROXY_CONF_FILE=/etc/nginx/conf.d/nginx.conf
 EMAIL_ARG="--register-unsafely-without-email"
 ADD_SERVER_NAME=""
 
 if [ $ENABLE_SSL == "TRUE" ]; then
     apk add certbot-nginx
 fi
-if [ -f "$FILE" ]; then
-    echo "Nginx configuration already exists: $FILE "
+if [ -f "$PROXY_CONF_FILE" ]; then
+    echo "Nginx configuration already exists: $PROXY_CONF_FILE "
 else
-    echo "Creating Nginx configuration: $FILE"
+    echo "Creating Nginx configuration: $PROXY_CONF_FILE"
 
     [ ! -z ${SERVER_NAME+x} ] && export ADD_SERVER_NAME="server_name ${SERVER_NAME}";
-    envsubst '${ADD_SERVER_NAME} ${PLATFORM_API_ENDPOINT} ${STATS_API_ENDPOINT}' < $CONF_TMPL > $FILE
+    envsubst '${ADD_SERVER_NAME} ${PLATFORM_API_ENDPOINT} ${STATS_API_ENDPOINT}' < $CONF_TMPL > $PROXY_CONF_FILE
 
     rm /etc/nginx/conf.d/default.conf || echo "File '/etc/nginx/conf.d/default.conf' already deleted. OK"
     nginx -t
-    cat /etc/nginx/conf.d/nginx.conf
+    cat $PROXY_CONF_FILE
     nginx -s reload
 
     if [ ! "$ENABLE_SSL" == "TRUE" ]; then
@@ -42,12 +42,9 @@ else
       certbot install --cert-name $SERVER_NAME || certbot --nginx -d $SERVER_NAME --non-interactive --agree-tos $EMAIL_ARG
 
       nginx -t
-      cat /etc/nginx/conf.d/nginx.conf
+      cat $PROXY_CONF_FILE
       nginx -s reload
     fi
 fi
 
 nginx -g "daemon off;"
-if [ $ENABLE_SSL == "TRUE" ]; then
-    trap exit TERM; while :; do certbot renew; sleep 12h & wait ${!}; done;
-fi
