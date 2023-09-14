3613c34ccad56608dd16a4964e9847b264ac4620
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2023-09-14 15:59:39 +0300

    Added the terms in the glossary

--- src/app/pages/eosc-readiness-dashboard/eosc-readiness-2022/glossary/glossary-2022.component.ts
+++ src/app/pages/eosc-readiness-dashboard/eosc-readiness-2022/glossary/glossary-2022.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'glossary',
-  templateUrl: 'glossary-2022.component.html'
+  templateUrl: 'glossary-2022.component.html',
+  styleUrls: ['./glossary-2022.component.css'],
 })
 
 export class Glossary2022Component {
