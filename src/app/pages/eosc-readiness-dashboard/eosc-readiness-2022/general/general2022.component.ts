0c96543cbeeed1a5a10853b8bec4237764ee7756
jbalasis <cs2180008@di.uoa.gr>
2023-10-10 16:51:57 +0300

    added charts for gdp, gerd, income percentage

--- src/app/pages/eosc-readiness-dashboard/eosc-readiness-2022/general/general2022.component.ts
+++ src/app/pages/eosc-readiness-dashboard/eosc-readiness-2022/general/general2022.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {CountryTableData} from "../../../../../survey-tool/app/domain/country-table-data";
-import {EoscReadiness2022MapSubtitles} from "../eosc-readiness2022-map-subtitles";
+import {countriesNumbers, EoscReadiness2022MapSubtitles} from "../eosc-readiness2022-map-subtitles";
 import {EoscReadiness2022DataService} from "../../../services/eosc-readiness2022-data.service";
 import {StakeholdersService} from "../../../../../survey-tool/app/services/stakeholders.service";
 import {DataHandlerService} from "../../../services/data-handler.service";
 import {zip} from "rxjs/internal/observable/zip";
 import {RawData} from "../../../../../survey-tool/app/domain/raw-data";
 import {isNumeric} from "rxjs/internal-compatibility";
+import UIkit from "uikit";
+import {countries} from "../../../../../survey-tool/app/domain/countries";
 
 @Component({
   selector: 'app-general-2022',
@@ -17,6 +19,7 @@ import {isNumeric} from "rxjs/internal-compatibility";
 export class General2022Component implements OnInit {
 
   type: string = null;
+  fragment: string = null;
 
   countriesArray: string[] = [];
   toolTipData: Map<string, string>[] = [];
@@ -28,9 +31,8 @@ export class General2022Component implements OnInit {
   questionsDataArrayForBarChart: any[] = [];
   sumsArray: string[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private queryData: EoscReadiness2022DataService,
-              private stakeholdersService: StakeholdersService, private dataHandlerService: DataHandlerService) {
-  }
+  constructor(private route: ActivatedRoute, private queryData: EoscReadiness2022DataService,
+              private stakeholdersService: StakeholdersService, private dataHandlerService: DataHandlerService) {}
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -51,15 +53,18 @@ export class General2022Component implements OnInit {
         }
         if (params['type'] === 'investments') {
           this.getInvestmentsData();
+          this.route.fragment.subscribe(
+            fragment => {
+              this.fragment = fragment;
+              this.activateSwitcher(fragment);
+            }
+          );
         }
-      }
-    )
 
-    this.route.fragment.subscribe(
-      fragment => {
-        console.log(fragment);
       }
     );
+
+
   }
 
   getResearchersData() {
@@ -148,6 +153,34 @@ export class General2022Component implements OnInit {
     )
   }
 
+  getInvestmentsDataPercentage(pos: number, param: string) {
+    zip(
+      this.stakeholdersService.getEOSCSBCountries(),
+      this.queryData.getQuestion5(),
+      this.queryData.getQuestion5comment(),
+    ).subscribe(
+      res => {
+        this.questionsDataArray[pos] = this.questionsDataArrayForBarChart[pos] = this.dataHandlerService.covertRawDataToColorAxisMap(res[1]);
+        for (let i = 0; i < this.questionsDataArray[pos].length; i++) {
+          let country = countriesNumbers.find(
+            elem=> elem.id.toLowerCase() === this.questionsDataArray[pos][i][0]
+          );
+          if (country?.[param]) {
+            this.questionsDataArray[pos][i][1] = this.round(this.questionsDataArray[pos][i][1] / country[param] * 1000, 4);
+          } else {
+            this.questionsDataArray[pos].splice(i, 1);
+            i--;
+          }
+        }
+        let tempArr: string[] = [];
+        this.questionsDataArray[pos].forEach((data: string[]) => {tempArr.push(data[0]);});
+        this.countriesArray = res[0].map(element => {return element.toLowerCase()}).filter(element => !tempArr.includes(element));
+        this.toolTipData[pos] = this.dataHandlerService.covertRawDataGetText(res[2]);
+        // this.sumsArray[4] = this.calculateSum(res[1]);
+      }
+    )
+  }
+
   calculateSum(rawData: RawData): string {
     let sum = 0.0;
     for (const series of rawData.datasets) {
@@ -159,4 +192,37 @@ export class General2022Component implements OnInit {
     }
     return (Math.round((sum + Number.EPSILON) * 100) / 100).toString();
   }
+
+  activateSwitcher(fragment: string) {
+    if (!UIkit.switcher('#investmentsContent')){
+      setTimeout( ()=> {
+        this.activateSwitcher(fragment);
+      }, 100);
+    } else {
+      switch (fragment) {
+        case 'absolute':
+          UIkit.switcher('#investmentsContent').show(0);
+          break;
+        case 'gdp':
+          this.getInvestmentsDataPercentage(5, 'gdp');
+          UIkit.switcher('#investmentsContent').show(1);
+          break;
+        case 'gerd':
+          this.getInvestmentsDataPercentage(6, 'gerd');
+          UIkit.switcher('#investmentsContent').show(2);
+          break;
+        case 'income':
+          this.getInvestmentsDataPercentage(7, 'income');
+          UIkit.switcher('#investmentsContent').show(3);
+          break;
+      }
+    }
+  }
+
+
+  round(value: number, precision?: number) {
+    const multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
 }
