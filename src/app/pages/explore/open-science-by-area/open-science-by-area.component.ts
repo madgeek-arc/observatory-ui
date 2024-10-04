37d69fd9a80f38a5d1670b6b66d8a3d4b0c0ca65
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2024-10-04 12:02:52 +0300

    Finalised the first version of the home page (with icons and disabled links, etc)

--- src/app/pages/explore/open-science-by-area/open-science-by-area.component.ts
+++ src/app/pages/explore/open-science-by-area/open-science-by-area.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: 'app-open-science-by-area',
   templateUrl: './open-science-by-area.component.html',
-  styles: [".material-symbols-outlined { font-variation-settings: 'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 24 }"]
 })
 
 export class OpenScienceByAreaComponent implements OnInit {
