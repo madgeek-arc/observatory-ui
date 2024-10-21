d519a6edcdbe53c3bf4531d09b1186704ab52217
jbalasis <cs2180008@di.uoa.gr>
2024-10-21 14:16:53 +0300

    Added charts with new data at demo page

--- src/app/pages/eosc-readiness-dashboard/eosc-readiness-dynamic/demo/demo-charts.component.ts
+++ src/app/pages/eosc-readiness-dashboard/eosc-readiness-dynamic/demo/demo-charts.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, DestroyRef, inject, OnInit } from "@angular/core";
 import { zip } from "rxjs/internal/observable/zip";
 import { StakeholdersService } from "../../../../../survey-tool/app/services/stakeholders.service";
 import { DataHandlerService } from "../../../services/data-handler.service";
 import { EoscReadinessDataService } from "../../../services/eosc-readiness-data.service";
 import { RawData } from "../../../../../survey-tool/app/domain/raw-data";
 import { isNumeric } from "rxjs/internal-compatibility";
-import { countriesNumbers } from "../../eosc-readiness-2022/eosc-readiness2022-map-subtitles";
+import {
+  ColorPallet,
+  countriesNumbers,
+  EoscReadiness2022MapSubtitles
+} from "../../eosc-readiness-2022/eosc-readiness2022-map-subtitles";
 import { PointOptionsObject, SeriesBubbleOptions, SeriesOptionsType } from "highcharts";
 import * as Highcharts from "highcharts";
+import { CategorizedAreaData, Series } from "../../../../../survey-tool/app/domain/categorizedAreaData";
+import { latlong } from "../../../../../survey-tool/app/domain/countries-lat-lon";
+import { SurveyService } from "../../../../../survey-tool/app/services/survey.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { CountryTableData } from "../../../../../survey-tool/app/domain/country-table-data";
 
 @Component({
   selector: "demo-charts",
@@ -15,7 +24,17 @@ import * as Highcharts from "highcharts";
 })
 
 export class DemoChartsComponent implements OnInit {
-  year = '2022';
+  private destroyRef = inject(DestroyRef);
+  year = '2023';
+
+  countriesArray: string[] = [];
+  // tableAbsoluteDataArray: CountryTableData[][] = [];
+  tmpQuestionsDataArray: any[] = [];
+  questionsDataArray: any[] = [];
+  mapPointData: CountryTableData[];
+  toolTipData: Map<string, string>[] = [];
+  mapSubtitlesArray: string[][] = EoscReadiness2022MapSubtitles;
+
   treeGraph: PointOptionsObject[] = [];
   bubbleWithPlotLines = [{
     type: 'bubble',
@@ -258,10 +277,152 @@ export class DemoChartsComponent implements OnInit {
   labelFormat3 = '{value}%';
 
   constructor(private queryData: EoscReadinessDataService, private stakeholdersService: StakeholdersService,
-              private dataHandlerService: DataHandlerService) {}
+              private dataHandlerService: DataHandlerService, private surveyService: SurveyService) {}
 
   ngOnInit() {
     this.getRanges();
+    this.getImmediatePublicationsData();
+    this.getRetentionPublicationsData();
+    this.getLicensingPublicationsData();
+  }
+
+  getImmediatePublicationsData() {
+    zip(
+      this.stakeholdersService.getEOSCSBCountries(),
+      this.queryData.getQuestion(this.year, 'Question6.3'),
+      this.queryData.getQuestion(this.year, 'Question6.3.1'),
+      // this.queryData.getQuestionComment(this.year, 'Question6.3'),
+      // this.surveyService.getSurveyValidatedCountries('m-eosc-sb-2023')
+    ).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(
+      res => {
+        this.countriesArray = res[0];
+        // this.tableAbsoluteDataArray[0] = this.dataHandlerService.convertRawDataToTableData(res[1]);
+        this.tmpQuestionsDataArray[0] = this.dataHandlerService.convertRawDataToCategorizedAreasData(res[1]);
+        // this.participatingCountries[0] = this.dataHandlerService.convertRawDataForActivityGauge(res[1]);
+        // this.participatingCountriesPercentage[0] = Math.round((this.participatingCountries[0]/this.countriesArray.length + Number.EPSILON) * 100);
+        for (let i = 0; i < this.tmpQuestionsDataArray[0].series.length; i++) {
+          this.tmpQuestionsDataArray[0].series[i].data = this.tmpQuestionsDataArray[0].series[i].data.map(code => ({ code }));
+        }
+        // this.toolTipData[0] = this.dataHandlerService.covertRawDataGetText(res[3]);
+        this.mapPointData = this.dataHandlerService.convertRawDataToTableData(res[2]);
+        this.createMapDataFromCategorizationWithDots(0,0);
+
+      },
+      error => {console.error(error)},
+      () => {}
+    );
+  }
+
+  getRetentionPublicationsData() {
+    zip(
+      this.stakeholdersService.getEOSCSBCountries(),
+      this.queryData.getQuestion(this.year, 'Question6.4'),
+      this.queryData.getQuestion(this.year, 'Question6.4.1'),
+      // this.surveyService.getSurveyValidatedCountries('m-eosc-sb-2023')
+    ).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(
+      res => {
+        this.countriesArray = res[0];
+        // this.tableAbsoluteDataArray[0] = this.dataHandlerService.convertRawDataToTableData(res[1]);
+        this.tmpQuestionsDataArray[1] = this.dataHandlerService.convertRawDataToCategorizedAreasData(res[1]);
+        // this.participatingCountries[0] = this.dataHandlerService.convertRawDataForActivityGauge(res[1]);
+        // this.participatingCountriesPercentage[0] = Math.round((this.participatingCountries[0]/this.countriesArray.length + Number.EPSILON) * 100);
+        for (let i = 0; i < this.tmpQuestionsDataArray[1].series.length; i++) {
+          this.tmpQuestionsDataArray[1].series[i].data = this.tmpQuestionsDataArray[1].series[i].data.map(code => ({ code }));
+        }
+        this.mapPointData = this.dataHandlerService.convertRawDataToTableData(res[2]);
+        this.createMapDataFromCategorizationWithDots(1, 0);
+
+      },
+      error => {console.error(error)},
+      () => {}
+    );
+  }
+
+  getLicensingPublicationsData() {
+    zip(
+      this.stakeholdersService.getEOSCSBCountries(),
+      this.queryData.getQuestion(this.year, 'Question6.5'),
+      this.queryData.getQuestion(this.year, 'Question6.5.1'),
+      // this.surveyService.getSurveyValidatedCountries('m-eosc-sb-2023')
+    ).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(
+      res => {
+        this.countriesArray = res[0];
+        // this.tableAbsoluteDataArray[0] = this.dataHandlerService.convertRawDataToTableData(res[1]);
+        this.tmpQuestionsDataArray[2] = this.dataHandlerService.convertRawDataToCategorizedAreasData(res[1]);
+        // this.participatingCountries[0] = this.dataHandlerService.convertRawDataForActivityGauge(res[1]);
+        // this.participatingCountriesPercentage[0] = Math.round((this.participatingCountries[0]/this.countriesArray.length + Number.EPSILON) * 100);
+        for (let i = 0; i < this.tmpQuestionsDataArray[2].series.length; i++) {
+          this.tmpQuestionsDataArray[2].series[i].data = this.tmpQuestionsDataArray[2].series[i].data.map(code => ({ code }));
+        }
+        this.mapPointData = this.dataHandlerService.convertRawDataToTableData(res[2]);
+        this.createMapDataFromCategorizationWithDots(2, 0);
+
+      },
+      error => {console.error(error)},
+      () => {}
+    );
+  }
+
+
+  createMapDataFromCategorizationWithDots(index: number, mapCount: number) {
+    // this.mapSubtitles[index] = this.mapSubtitlesArray[mapCount][index];
+
+    this.questionsDataArray[index] = new CategorizedAreaData();
+
+    let position = 0;
+    for (let i = 0; i < this.tmpQuestionsDataArray[index].series.length; i++) {
+      if (this.tmpQuestionsDataArray[index].series[i].name === 'Awaiting data')
+        continue;
+      position = this.tmpQuestionsDataArray[index].series[i].name === 'No'? 1 : 0;
+      this.questionsDataArray[index].series[i] = new Series(this.mapSubtitlesArray[mapCount][position], false);
+      this.questionsDataArray[index].series[i].data = this.tmpQuestionsDataArray[index].series[i].data;
+      this.questionsDataArray[index].series[i].showInLegend = true;
+      this.questionsDataArray[index].series[i].color = ColorPallet[position];
+    }
+    let countryCodeArray = [];
+    for (let i = 0; i < this.questionsDataArray[index].series.length; i++) {
+      for (const data of this.questionsDataArray[index].series[i].data) {
+        countryCodeArray.push(data.code)
+      }
+    }
+
+    if (countryCodeArray.length > 0) {
+      this.questionsDataArray[index].series[this.questionsDataArray[index].series.length] = new Series('Awaiting Data', false);
+      this.questionsDataArray[index].series[this.questionsDataArray[index].series.length-1].showInLegend = true;
+      this.questionsDataArray[index].series[this.questionsDataArray[index].series.length-1].color = ColorPallet[2];
+      this.questionsDataArray[index].series[this.questionsDataArray[index].series.length-1].data = this.countriesArray.filter(code => !countryCodeArray.includes(code));
+      this.questionsDataArray[index].series[this.questionsDataArray[index].series.length-1].data = this.questionsDataArray[index].series[this.questionsDataArray[index].series.length-1].data.map(code => ({ code }));
+    }
+
+    let mapPointArray1 = [];
+    let mapPointArray2 = [];
+    for (let i = 0; i < this.mapPointData.length; i++) {
+      if (this.mapPointData[i].dedicatedFinancialContributionsToEOSCLinkedToPolicies === 'Yes') {
+        mapPointArray1.push({name: this.mapPointData[i].code, lat: latlong.get(this.mapPointData[i].code).latitude, lon: latlong.get(this.mapPointData[i].code).longitude});
+      } else if (this.mapPointData[i].dedicatedFinancialContributionsToEOSCLinkedToPolicies === 'No') {
+        mapPointArray2.push({name: this.mapPointData[i].code, lat: latlong.get(this.mapPointData[i].code).latitude, lon: latlong.get(this.mapPointData[i].code).longitude});
+      }
+    }
+
+    let pos: number;
+    if (mapPointArray1.length > 0) {
+      pos = this.questionsDataArray[index].series.length;
+      this.questionsDataArray[index].series[pos] = new Series('National policy is mandatory', false, 'mappoint');
+      this.questionsDataArray[index].series[pos].data = mapPointArray1;
+      this.questionsDataArray[index].series[pos].color = '#7CFC00';
+      this.questionsDataArray[index].series[pos].marker.symbol = 'circle';
+      this.questionsDataArray[index].series[pos].showInLegend = true;
+    }
+
+    if (mapPointArray2.length > 0) {
+      pos = this.questionsDataArray[index].series.length;
+      this.questionsDataArray[index].series[pos] = new Series('National policy is not mandatory', false, 'mappoint');
+      this.questionsDataArray[index].series[pos].data = mapPointArray2;
+      this.questionsDataArray[index].series[pos].color = '#FFEF00';
+      this.questionsDataArray[index].series[pos].marker.symbol = 'diamond';
+      this.questionsDataArray[index].series[pos].showInLegend = true;
+    }
+
   }
 
   getRanges() {
