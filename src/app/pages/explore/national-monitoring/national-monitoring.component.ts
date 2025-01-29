6a355ddc0d55c095b1cd10e398cec875209a1823
jbalasis <cs2180008@di.uoa.gr>
2025-01-29 17:42:49 +0200

    feat: Multi chart structure at explore -> monitoring

--- src/app/pages/explore/national-monitoring/national-monitoring.component.ts
+++ src/app/pages/explore/national-monitoring/national-monitoring.component.ts
@@ -9,7 +9,6 @@ import { countries } from "../../../../survey-tool/app/domain/countries";
 import { SurveyService } from "../../../../survey-tool/app/services/survey.service";
 import { PdfExportService } from "../../services/pdf-export.service";
 import { StakeholdersService } from "../../../../survey-tool/app/services/stakeholders.service";
-import { CountryTableData } from "../../../../survey-tool/app/domain/country-table-data";
 import { ExploreService } from "../explore.service";
 
 @Component({
@@ -37,14 +36,14 @@ export class NationalMonitoringComponent implements OnInit {
     yAxis: 'Percentage of countries with National Monitoring',
   }
 
-  navPills = ['Publications', 'Open Data', 'FAIR Data', 'Data Management', 'Citizen Science', 'Repositories', 'Long-term Data', 'Training', 'Software'];
+  navPills = ['Publications', 'Open Data', 'FAIR Data', 'Data Management', 'Citizen Science', 'Repositories', 'Long-term Data Preservation', 'Skills/Training', 'Software'];
+  mapTitles = ['National monitoring on open access publications', 'National Monitoring on Open Data', 'National Monitoring on FAIR Data', 'National Monitoring on Data Management', 'National Monitoring on Citizen Science', 'National Monitoring on Connecting Repositories to EOSC', 'National Monitoring on Long-term Data Preservation', 'National Monitoring on Skills/Training in Open Science', 'National Monitoring on Open Sources Software']
 
   countriesArray: string[] = [];
   questionsDataArray: any[] = [];
   tmpQuestionsDataArray: any[] = [];
   participatingCountries: number[] = [];
   total: number[] = [];
-  mapPointData: CountryTableData[];
   toolTipData: Map<string, string>[] = [];
 
   constructor(private queryData: EoscReadinessDataService, private surveyService: SurveyService,
@@ -62,9 +61,15 @@ export class NationalMonitoringComponent implements OnInit {
     this.stakeholdersService.getEOSCSBCountries().pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
       next: countries => {
         this.countriesArray = countries;
-        this.getMonitoring('Question98', 0, 2);
-        this.getMonitoring('Question98', 1, 2);
-        this.getMonitoring('Question98', 2, 2);
+        this.getMonitoring('Question54', 0, 2); // National monitoring on open access publications
+        // this.getMonitoring('Question66', 1, 2); // National Monitoring on Open Data
+        // this.getMonitoring('Question62', 2, 2); // National Monitoring on FAIR Data
+        // this.getMonitoring('Question58', 3, 2); // National Monitoring on Data Management
+        // this.getMonitoring('Question98', 4, 2); // National Monitoring on Citizen Science
+        // this.getMonitoring('Question78', 5, 2); // National Monitoring on Connecting Repositories to EOSC
+        // this.getMonitoring('Question86', 6, 2); // National Monitoring on Long-term Data Preservation
+        // this.getMonitoring('Question90', 7, 2); // National Monitoring on Skills/Training in Open Science
+        // this.getMonitoring('Question70', 8, 2); // National Monitoring on Open Sources Software
       },
       error: error => {console.error(error);}
     });
@@ -89,6 +94,48 @@ export class NationalMonitoringComponent implements OnInit {
       },
       error: err => {console.error(err)}
     });
+  }
+
+  getChart(index: number) {
+    console.log(this.questionsDataArray);
+    switch (index) {
+      case 0:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question54', index, 2); // National monitoring on open access publications
+        break;
+      case 1:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question66', index, 2); // National Monitoring on Open Data
+        break;
+      case 2:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question62', index, 2); // National Monitoring on FAIR Data
+        break;
+      case 3:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question58', index, 2); // National Monitoring on Data Management
+        break;
+      case 4:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question98', index, 2); // National Monitoring on Citizen Science
+        break;
+      case 5:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question78', index, 2); // National Monitoring on Connecting Repositories to EOSC
+        break;
+      case 6:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question86', index, 2); // National Monitoring on Long-term Data Preservation
+        break;
+      case 7:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question90', index, 2); // National Monitoring on Skills/Training in Open Science
+        break;
+      case 8:
+        if (!this.questionsDataArray[index])
+          this.getMonitoring('Question70', index, 2); // National Monitoring on Open Sources Software
+        break;
+    }
   }
 
   /** Bar charts ---------------------------------------------------------------------------------------------------> **/
