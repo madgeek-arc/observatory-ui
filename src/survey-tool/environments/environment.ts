399c41d4c2f2581b23fcf2c8ff6abba9d704c5a3
jBalasis <j.balasis92@gmail.com>
2025-02-27 11:32:51 +0200

    chore: removed survey-tool as subrepo

--- src/survey-tool/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// This file points to outer repository environment, it's for catalogue-ui compatibility.
-import {environment as environmentProperties} from "../../environments/environment";
-
-
-export const environment = environmentProperties;
