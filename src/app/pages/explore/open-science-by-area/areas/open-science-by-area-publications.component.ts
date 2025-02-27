f27ba719190f26adbfbfbb9ade18a27d29381c2b
jBalasis <j.balasis92@gmail.com>
2025-02-27 16:53:27 +0200

    feat: display demo bar chart with duplicate xAxis on OS by area at publication section

--- src/app/pages/explore/open-science-by-area/areas/open-science-by-area-publications.component.ts
+++ src/app/pages/explore/open-science-by-area/areas/open-science-by-area-publications.component.ts
@@ -9,7 +9,13 @@ import {
   trendOfOAPublications
 } from "../../OSO-stats-queries/explore-queries";
 import * as Highcharts from "highcharts";
-import { LegendOptions, OptionsStackingValue, PointOptionsObject, SeriesBarOptions } from "highcharts";
+import {
+  LegendOptions,
+  OptionsStackingValue,
+  PointOptionsObject,
+  SeriesBarOptions,
+  SeriesOptionsType
+} from "highcharts";
 import { PdfExportService } from "../../../services/pdf-export.service";
 import { zip } from "rxjs/internal/observable/zip";
 import { StakeholdersService } from "../../../../../survey-tool/app/services/stakeholders.service";
@@ -134,6 +140,30 @@ export class OpenScienceByAreaPublicationsComponent implements OnInit {
   toolTipData: Map<string, string>[] = [];
   comment?: string;
   countryName?: string;
+
+  bar2: SeriesOptionsType[] = [
+    {
+      type: 'bar',
+      name: 'Male',
+      data: [
+        -1.38, -2.09, -2.45, -2.71, -2.97,
+        -3.69, -4.04, -3.81, -4.19, -4.61,
+        -4.56, -4.21, -3.53, -2.55, -1.82,
+        -1.46, -0.78, -0.71
+      ]
+    },
+    {
+      type: 'bar',
+      name: 'Female',
+      data: [
+        1.35, 1.98, 2.43, 2.39, 2.71,
+        3.02, 3.50, 3.52, 4.03, 4.40,
+        4.17, 3.88, 3.29, 2.42, 1.80,
+        1.39, 0.99, 1.15
+      ]
+    }
+  ];
+  barCategories: string[] = ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-40', '40-45', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80-84', '80+'];
 
   constructor(private queryData: EoscReadinessDataService, private pdfService: PdfExportService,
               private stakeholdersService: StakeholdersService, private dataHandlerService: DataHandlerService,
