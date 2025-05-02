6eca7f08ab1a5cd98f240a7abaf6eb6df0c1c812
jbalasis <cs2180008@di.uoa.gr>
2025-05-02 16:46:02 +0300

    fix: replace data for bubble chart at OS policies section

--- src/app/pages/explore/open-science-policies/open-science-policies.component.ts
+++ src/app/pages/explore/open-science-policies/open-science-policies.component.ts
@@ -298,10 +298,10 @@ export class OpenSciencePoliciesComponent implements OnInit {
   /** Bubble chart ------------------------------------------------------------------------------------------------> **/
   getBubbleChartData() {
     zip(
-      this.queryData.getQuestion(this.year, 'Question6'),   // national policy on open access publications
-      this.queryData.getQuestion(this.year, 'Question6.1'), // national policy is mandatory
-      this.queryData.getQuestion(this.year, 'Question56'),  // financial investments in open access publications
-      this.queryData.getQuestion(this.year, 'Question57'),  // number of publications published in open access
+      this.queryData.getQuestion(this.year, 'Question1'),   // Number of Researchers in FTE per country
+      this.queryData.getQuestion(this.year, 'Question5'),   // Financial investments in EOSC and Open Science per country
+      this.queryData.getQuestion(this.year, 'Question56'),  // Financial investments in open access publications
+      // this.queryData.getQuestion(this.year, 'Question57'),  // number of publications published in open access
     ).pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
       next: value => {
         // console.log(value);
@@ -313,40 +313,21 @@ export class OpenSciencePoliciesComponent implements OnInit {
   }
 
   createBubbleChartSeries(data: RawData[]) {
-    const tmpArr = this.mergeArrays(data[0].datasets[0].series.result, data[1].datasets[0].series.result, data[2].datasets[0].series.result, data[3].datasets[0].series.result);
+    const tmpArr = this.mergeArrays(data[0].datasets[0].series.result, data[1].datasets[0].series.result, data[2].datasets[0].series.result);
     // console.log(tmpArr);
     let series = [
-      {
-        name: 'Policy is mandatory',
-        // color: '#32cd32',
-        data: []
-      }, {
-        name: 'Policy is not mandatory',
-        // color: '#ff8c00',
-        data: []
-      }, {
-        name: 'No policy',
-        // color: '#808080',
-        data: []
-      }
+      {data: []}
     ] as SeriesBubbleOptions[];
 
     tmpArr.forEach(el => {
 
-      if (!this.isNumeric(el[3]) || !this.isNumeric(el[4]))
+      if (!this.exploreService.isNumeric(el[1]) || !this.exploreService.isNumeric(el[2]))
         return;
 
       //TODO: Z is statically set to 10 until corresponding query is provided
-      let item = {x: +el[4], y: this.rangeSelector(+el[3]), z: 10, name: el[0], country: this.findCountryName(el[0]).name};
+      let item = {x: +el[1], y: +el[2], z: 10, name: el[0], country: this.findCountryName(el[0]).name};
 
-      if (el[1] === 'No')
-        series[2].data.push(item);
-      else if (el[1] === 'Yes') {
-        if(el[2] === 'Yes')
-          series[0].data.push(item);
-        else if (el[2] === 'No')
-          series[1].data.push(item);
-      }
+      series[0].data.push(item);
 
     });
     return series;
@@ -429,35 +410,6 @@ export class OpenSciencePoliciesComponent implements OnInit {
     return countriesNumbers.find(
       elem => elem.id === code
     );
-  }
-
-  isNumeric(value: string | null): boolean {
-    // Check if the value is empty
-    if (value === null)
-      return false;
-
-    if (value.trim() === '') {
-      return false;
-    }
-
-    // Attempt to parse the value as a float
-    const number = parseFloat(value);
-
-    // Check if parsing resulted in NaN or the value has extraneous characters
-    return !isNaN(number) && isFinite(number) && String(number) === value;
-  }
-
-  rangeSelector(value: number) {
-    if (value < 1)
-      return 0;
-    else if (value < 5)
-      return 1;
-    else if (value < 10)
-      return 2;
-    else if (value < 20)
-      return 3;
-    else
-      return 4;
   }
 
 }
