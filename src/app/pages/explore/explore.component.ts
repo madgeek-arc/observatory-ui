10084c88b938b37d1e3f177ff73b8dcb1688a438
jBalasis <j.balasis92@gmail.com>
2025-06-24 17:01:57 +0300

    wip

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,14 +1,32 @@
-import { Component} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { MenuItem, SideBarComponent } from "../../shared/side-bar/side-bar.component";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-explore',
+  standalone: true,
+  imports: [
+    SideBarComponent,
+    RouterOutlet,
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './explore.component.html'
 })
 
-export class ExploreComponent {
+export class ExploreComponent implements OnInit {
+
+  menuItems: MenuItem[] = [];
 
   hideSubNavigation: boolean = true;
   activeSection: string = null;
+
+  ngOnInit() {
+    this.menuItems.push(new MenuItem('0','Open Science by Area', 'open-science-by-area', 'open-science-by-area', 'open-science-by-area'));
+    this.menuItems[0].items.push(new MenuItem('0-0', 'Publications', 'publications', 'publications'));
+    this.menuItems.push(new MenuItem('1', 'Open Science Trends', 'open-science-trends', 'open-science-trends'));
+  }
 
   toggleAreaSubNav() {
     this.hideSubNavigation = !this.hideSubNavigation;
