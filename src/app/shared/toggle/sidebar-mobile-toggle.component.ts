97f44603bb317a97e1b73b3ef0cd093ce2b12ae9
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2025-06-27 13:27:34 +0300

    chore: removed static sidebar item string from mobile toggle

--- src/app/shared/toggle/sidebar-mobile-toggle.component.ts
+++ src/app/shared/toggle/sidebar-mobile-toggle.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from "@angular/core";
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
 import { CommonModule } from "@angular/common";
+import {SidebarItem, SideBarService} from "../side-bar/side-bar-service";
+import {Subscription} from "rxjs";
 
 @Component({
   standalone: true,
@@ -7,19 +9,43 @@ import { CommonModule } from "@angular/common";
   selector: 'sidebar-mobile-toggle',
   template: `
     <a *ngIf="activeSidebarItem" href="#sidebar_offcanvas" class="sidebar_mobile_toggle uk-link-reset uk-width-3-5 uk-flex uk-flex-middle" uk-toggle>
+<!--      <div *ngIf="activeSidebarItem.icon && (activeSidebarItem.icon.svg || activeSidebarItem.icon.name)" class="uk-width-auto">-->
+<!--        <icon class="menu-icon" [customClass]="activeSidebarItem.icon.class" [name]="activeSidebarItem.icon.name" ratio="0.8" [svg]="activeSidebarItem.icon.svg" [flex]="true"></icon>-->
+<!--      </div>-->
       <span class="uk-width-expand uk-text-truncate uk-margin-small-left uk-text-bolder">
-        {{ activeSidebarItem }}
-        <span *ngIf="activeSidebarSubItem">- {{ activeSidebarSubItem }}</span>
+          {{activeSidebarItem.name}}
+        <span *ngIf="activeSidebarItem.subItem">- {{activeSidebarItem.subItem.name}}</span>
       </span>
-      <!-- <div class="uk-width-auto uk-margin-small-left">
-        <icon name="arrow_right" ratio="1.4" [flex]="true"></icon>
-      </div> -->
+      <div class="uk-width-auto uk-margin-small-left">
+<!--        <icon name="arrow_right" ratio="1.4"  [flex]="true"></icon>-->
+      </div>
     </a>
   `
 })
-export class SidebarMobileToggleComponent {
-  @Input() activeSidebarItem: string;
+export class SidebarMobileToggleComponent implements OnInit, OnDestroy {
+  @Input() activeSidebarItemStatic: string;
   @Input() activeSidebarSubItem: string;
+
+  public activeSidebarItem: SidebarItem;
+  private subscriptions: any[] = [];
+
+  constructor(private layoutService: SideBarService) {
+  }
+
+  ngOnInit() {
+    this.subscriptions.push(this.layoutService.activeSidebarItem.subscribe(activeSidebarItem => {
+      console.log('activeSidebarItem', activeSidebarItem);
+      this.activeSidebarItem = activeSidebarItem;
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => {
+      if(subscription instanceof Subscription) {
+        subscription.unsubscribe();
+      }
+    });
+  }
+
 }
 
- 
