e476a5ca9745d4d7d8e8740486ed5fee7c1e55fb
jbalasis <cs2180008@di.uoa.gr>
2025-07-23 18:36:51 +0300

    chore: more package version testing

--- src/app/shared/charts/world-map/world-map.component.ts
+++ src/app/shared/charts/world-map/world-map.component.ts
@@ -1,11 +1,15 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import * as Highcharts from 'highcharts/highmaps';
-import HC_map from 'highcharts/modules/map';
 import worldMapData from '@highcharts/map-collection/custom/world-highres3.topo.json';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { NgIf } from "@angular/common";
+import HC_map from 'highcharts/modules/map';
+import Exporting from 'highcharts/modules/exporting';
+import HC_ExportingOffline from 'highcharts/modules/offline-exporting';
 
 HC_map(Highcharts);
+Exporting(Highcharts);
+HC_ExportingOffline(Highcharts);
 
 @Component({
   selector: 'app-world-map',
@@ -17,10 +21,14 @@ HC_map(Highcharts);
   template: `<highcharts-chart *ngIf="ready" [Highcharts]="Highcharts" [constructorType]="'mapChart'" [options]="chartOptions" style="width: 100%; display: block;"></highcharts-chart>`
 })
 export class WorldMapComponent implements OnInit, AfterViewInit {
-  Highcharts: typeof Highcharts = Highcharts;
-  colorPallet = ['#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', '#A9A9A9'];
 
+  @Output() chartReady = new EventEmitter<Highcharts.Chart>();
+
+
+  Highcharts: typeof Highcharts = Highcharts;
   chartOptions: Highcharts.Options = {}
+
+  colorPallet = ['#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', '#A9A9A9'];
   ready = false;
 
   ngOnInit() {
@@ -30,11 +38,18 @@ export class WorldMapComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {}
 
   createMap() {
+    const that = this;
     this.ready = false;
 
     this.chartOptions = {
       chart: {
-        map: worldMapData
+        map: worldMapData,
+        events: {
+          load(this: Highcharts.Chart) {
+            // `this` is already typed as the Chart instance
+            that.chartReady.emit(this);
+          }
+        }
       },
       mapView: {
         center: [15, 50],
@@ -42,6 +57,9 @@ export class WorldMapComponent implements OnInit, AfterViewInit {
       },
       title: {
         text: undefined
+      },
+      credits: {
+        enabled: false
       },
       colorAxis: {
         dataClasses: [
@@ -51,7 +69,8 @@ export class WorldMapComponent implements OnInit, AfterViewInit {
         ]
       },
       exporting: {
-        enabled: false
+        enabled: true,
+
       },
       plotOptions: {
         map: {
@@ -92,4 +111,18 @@ export class WorldMapComponent implements OnInit, AfterViewInit {
 
     this.ready = true;
   }
+
+  /** Helper to emit base64 PNG up to parent */
+  // public async emitImage() {
+  //   if (!this.chartInstance) { return; }
+  //   // requires offline-exporting module
+  //   const canvas = this.chartInstance.createCanvas();
+  //   const dataURL = canvas.toDataURL('image/png');
+  //   // strip off the prefix if you want raw base64:
+  //   const base64 = dataURL.split(',')[1];
+  //   this.chartReady.emit(this.chartInstance);     // or another @Output for image
+  //   // OR: emit a string:
+  //   // this.imageReady.emit(dataURL);
+  // }
+
 }
