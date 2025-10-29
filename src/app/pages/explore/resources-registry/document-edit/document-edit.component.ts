1d7b732151a00718644a125a1e619bfae05841fa
Vasilis <billnick@hotmail.gr>
2025-10-29 16:49:41 +0200

    feat(document-edit): Implement full FormArray ts logic and build html structure

--- src/app/pages/explore/resources-registry/document-edit/document-edit.component.ts
+++ src/app/pages/explore/resources-registry/document-edit/document-edit.component.ts
@@ -3,10 +3,9 @@ import { ActivatedRoute } from "@angular/router";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { ResourceRegistryService } from "src/app/pages/explore/resources-registry/resource-registry.service";
 import { CommonModule } from "@angular/common";
-import { Author, Document, Link } from "src/app/domain/document";
+import { Author, Document } from "src/app/domain/document";
 import { FormArray } from "@angular/forms";
-import { Description } from "src/app/domain/document";
-import { Content } from "src/app/domain/document";
+
 
 @Component({
   selector: 'app-document-edit',
@@ -51,13 +50,23 @@ export class DocumentEditComponent implements OnInit {
     createForm(): void {
         this.editForm = new FormGroup({
             title: new FormControl<string | null>(null, Validators.required),
+            originalTitles: new FormArray([]),
             acronym: new FormControl<string | null>(null),
             country: new FormControl<string | null>(null),
             language: new FormControl<string | null>(null),
             startDate: new FormControl<string | null>(null),
             endDate: new FormControl<string | null>(null),
-            author: new FormArray([]),
-            shortDescription: this.createDescriptionGroup()
+            publicationDate: new FormControl<string | null>(null),
+            modificationDate: new FormControl<string | null>(null),
+            organisations: new FormArray([]),
+            authors: new FormArray([this.createAuthorGroup()]),
+            links: new FormArray([]),
+            otherLinks: new FormArray([]),
+            tags: new FormArray([]),
+            otherTags: new FormArray([]),
+            shortDescription: this.createDescriptionGroup(),
+            abstract: this.createDescriptionGroup(),
+            summary: this.createDescriptionGroup(),
         });
     }
 
@@ -75,10 +84,21 @@ export class DocumentEditComponent implements OnInit {
         })
     }
 
+    createLinkGroup(): FormGroup {
+        return new FormGroup({
+            name: new FormControl<string | null>(null),
+            pid: new FormControl<string | null>(null),
+            type:  new FormControl<string | null>(null),
+            url:  new FormControl<string | null>(null),
+            description:  new FormControl<string | null>(null)
+        })
+    }
+
     // --------------------------
     //GETTERS 
     // --------------------------
 
+    //Authors
     get authors(): FormArray {
         return this.editForm.get('authors') as FormArray;
     }
@@ -89,6 +109,84 @@ export class DocumentEditComponent implements OnInit {
 
     removeAuthor(index: number ): void {
         this.authors.removeAt(index);
+    }
+
+    //Original Titles
+    get originalTitles(): FormArray {
+        return this.editForm.get('originalTitles') as FormArray;
+    }
+
+    addOriginalTitle(): void {
+        this.originalTitles.push(new FormControl<string | null>(null));
+    }
+
+    removeOriginalTitle(index: number): void {
+        this.originalTitles.removeAt(index);
+    }
+
+    //Organisations
+    get organisations(): FormArray {
+        return this.editForm.get('organisations') as FormArray
+    }
+
+    addOrganisation(): void {
+        this.organisations.push(new FormControl<string | null>(null));
+    }
+
+    removeOrganisations(index: number): void {
+        this.organisations.removeAt(index);
+    }
+
+    //Tags 
+    get tags(): FormArray {
+        return this.editForm.get('tags') as FormArray
+    }
+
+    addTag(): void {
+        this.tags.push(new FormControl<string | null>(null));
+    }
+
+    removeTags(index: number): void {
+        this.tags.removeAt(index);
+    }
+
+    //Other tags
+    get otherTags(): FormArray {
+        return this.editForm.get('otherTags') as FormArray
+    }
+
+    addOtherTag(): void {
+        this.otherTags.push(new FormControl<string | null>(null));
+    }
+
+    removeOtherTags(index: number): void {
+        this.otherTags.removeAt(index)
+    }
+
+    //Links
+    get links(): FormArray {
+        return this.editForm.get('links') as FormArray
+    }
+
+    addLink(): void {
+        this.links.push(this.createLinkGroup());
+    }
+
+    removeLink(index: number): void {
+        this.links.removeAt(index)
+    }
+
+    //Other Links
+    get otherLinks(): FormArray {
+        return this.editForm.get('otherLinks') as FormArray
+    }
+
+    addOtherLink(): void {
+        this.otherLinks.push(this.createLinkGroup())
+    }
+
+    removeOtherLink(index: number): void {
+        this.otherLinks.removeAt(index)
     }
 
 }
